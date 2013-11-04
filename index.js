4d8f4d41e02fde4594c32454daaf9df08485a1b7
Oliver Joseph Ash <oliverjash@icloud.com>
2013-11-04 17:16:52 +0000

    Use AMD with node adapter (UMD)

--- index.js
+++ index.js
@@ -1,138 +1,148 @@
-// Source: http://jsfiddle.net/vWx8V/
-// http://stackoverflow.com/questions/5603195/full-list-of-javascript-keycodes
+// UMD, as per https://github.com/umdjs/umd/blob/master/nodeAdapter.js
+if (typeof module === 'object' && typeof define !== 'function') {
+    var define = function (factory) {
+        module.exports = factory(require, exports, module);
+    };
+}
 
-var has = ({}).hasOwnProperty
+define(function (require, exports, module) {
+  // Source: http://jsfiddle.net/vWx8V/
+  // http://stackoverflow.com/questions/5603195/full-list-of-javascript-keycodes
 
-/**
- * Conenience method returns corresponding value for given keyName or keyCode.
- *
- * @param {Mixed} keyCode {Number} or keyName {String}
- * @return {Mixed}
- * @api public
- */
+  var has = ({}).hasOwnProperty
 
-exports = module.exports = function(searchInput) {
-  // Keyboard Events
-  if (searchInput && 'object' === typeof searchInput) {
-    var hasKeyCode = searchInput.which || searchInput.keyCode || searchInput.charCode
-    if (hasKeyCode) searchInput = hasKeyCode
+  /**
+   * Conenience method returns corresponding value for given keyName or keyCode.
+   *
+   * @param {Mixed} keyCode {Number} or keyName {String}
+   * @return {Mixed}
+   * @api public
+   */
+  function keycode(searchInput) {
+    // Keyboard Events
+    if (searchInput && 'object' === typeof searchInput) {
+      var hasKeyCode = searchInput.which || searchInput.keyCode || searchInput.charCode
+      if (hasKeyCode) searchInput = hasKeyCode
+    }
+
+    // Numbers
+    if ('number' === typeof searchInput) return names[searchInput]
+
+    // Everything else (cast to string)
+    var search = String(searchInput)
+
+    // check codes
+    var foundNamedKey = codes[search.toLowerCase()]
+    if (foundNamedKey) return foundNamedKey
+
+    // check aliases
+    var foundNamedKey = aliases[search.toLowerCase()]
+    if (foundNamedKey) return foundNamedKey
+
+    // weird character?
+    if (search.length === 1) return search.charCodeAt(0)
+
+    return undefined
   }
 
-  // Numbers
-  if ('number' === typeof searchInput) return names[searchInput]
+  /**
+   * Get by name
+   *
+   *   keycode.code['enter'] // => 13
+   */
 
-  // Everything else (cast to string)
-  var search = String(searchInput)
+  var codes = keycode.code = keycode.codes = {
+    'backspace': 8,
+    'tab': 9,
+    'enter': 13,
+    'shift': 16,
+    'ctrl': 17,
+    'alt': 18,
+    'pause/break': 19,
+    'caps lock': 20,
+    'esc': 27,
+    'space': 32,
+    'page up': 33,
+    'page down': 34,
+    'end': 35,
+    'home': 36,
+    'left': 37,
+    'up': 38,
+    'right': 39,
+    'down': 40,
+    'insert': 45,
+    'delete': 46,
+    'windows': 91,
+    'right click': 93,
+    'numpad *': 106,
+    'numpad +': 107,
+    'numpad -': 109,
+    'numpad .': 110,
+    'numpad /': 111,
+    'num lock': 144,
+    'scroll lock': 145,
+    'my computer': 182,
+    'my calculator': 183,
+    ';': 186,
+    '=': 187,
+    ',': 188,
+    '-': 189,
+    '.': 190,
+    '/': 191,
+    '`': 192,
+    '[': 219,
+    '\\': 220,
+    ']': 221,
+    "'": 222
+  }
 
-  // check codes
-  var foundNamedKey = codes[search.toLowerCase()]
-  if (foundNamedKey) return foundNamedKey
+  // Helper aliases
 
-  // check aliases
-  var foundNamedKey = aliases[search.toLowerCase()]
-  if (foundNamedKey) return foundNamedKey
-
-  // weird character?
-  if (search.length === 1) return search.charCodeAt(0)
-
-  return undefined
-}
-
-/**
- * Get by name
- *
- *   exports.code['enter'] // => 13
- */
-
-var codes = exports.code = exports.codes = {
-  'backspace': 8,
-  'tab': 9,
-  'enter': 13,
-  'shift': 16,
-  'ctrl': 17,
-  'alt': 18,
-  'pause/break': 19,
-  'caps lock': 20,
-  'esc': 27,
-  'space': 32,
-  'page up': 33,
-  'page down': 34,
-  'end': 35,
-  'home': 36,
-  'left': 37,
-  'up': 38,
-  'right': 39,
-  'down': 40,
-  'insert': 45,
-  'delete': 46,
-  'windows': 91,
-  'right click': 93,
-  'numpad *': 106,
-  'numpad +': 107,
-  'numpad -': 109,
-  'numpad .': 110,
-  'numpad /': 111,
-  'num lock': 144,
-  'scroll lock': 145,
-  'my computer': 182,
-  'my calculator': 183,
-  ';': 186,
-  '=': 187,
-  ',': 188,
-  '-': 189,
-  '.': 190,
-  '/': 191,
-  '`': 192,
-  '[': 219,
-  '\\': 220,
-  ']': 221,
-  "'": 222
-}
-
-// Helper aliases
-
-var aliases = exports.aliases = {
-  'ctl': 17,
-  'pause': 19,
-  'break': 19,
-  'caps': 20,
-  'escape': 27,
-  'pgup': 33,
-  'pgdn': 33,
-  'ins': 45,
-  'del': 46,
-  'spc': 32
-}
+  var aliases = keycode.aliases = {
+    'ctl': 17,
+    'pause': 19,
+    'break': 19,
+    'caps': 20,
+    'escape': 27,
+    'pgup': 33,
+    'pgdn': 33,
+    'ins': 45,
+    'del': 46,
+    'spc': 32
+  }
 
 
-/*!
- * Programatically add the following
- */
+  /*!
+   * Programatically add the following
+   */
 
-// lower case chars
-for (i = 97; i < 123; i++) codes[String.fromCharCode(i)] = i - 32
+  // lower case chars
+  for (i = 97; i < 123; i++) codes[String.fromCharCode(i)] = i - 32
 
-// numbers
-for (var i = 48; i < 58; i++) codes[i - 48] = i
+  // numbers
+  for (var i = 48; i < 58; i++) codes[i - 48] = i
 
-// function keys
-for (i = 1; i < 13; i++) codes['f'+i] = i + 111
+  // function keys
+  for (i = 1; i < 13; i++) codes['f'+i] = i + 111
 
-// numpad keys
-for (i = 0; i < 10; i++) codes['numpad '+i] = i + 96
+  // numpad keys
+  for (i = 0; i < 10; i++) codes['numpad '+i] = i + 96
 
-/**
- * Get by code
- *
- *   exports.name[13] // => 'Enter'
- */
+  /**
+   * Get by code
+   *
+   *   keycode.name[13] // => 'Enter'
+   */
 
-var names = exports.names = exports.title = {} // title for backward compat
+  var names = keycode.names = keycode.title = {} // title for backward compat
 
-// Create reverse mapping
-for (i in codes) names[codes[i]] = i
+  // Create reverse mapping
+  for (i in codes) names[codes[i]] = i
 
-// Add aliases
-for (var alias in aliases) {
-  codes[alias] = aliases[alias]
-}
+  // Add aliases
+  for (var alias in aliases) {
+    codes[alias] = aliases[alias]
+  }
+
+  return keycode;
+});
